aa1457fe32da77fe5e1a7c89d7f74ba05dc20388
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-28 11:39:38 -0700

    printf_pointer.c

---
+++ printf_pointer.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * printf_pointer - prints an hexdecimal number.
+ * @val: arguments of printf.
+ * Return: counter.
+ */
+int printf_pointer(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int i;
+	int j;
+	int y;
+
+	p = va_arg(val, void*);
+	if (p == NULL)
+	{
+		for (y = 0; s[y] != '\0'; y++)
+		{
+			_putchar(s[y]);
+		}
+		return (y);
+	}
+
+	y = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	j = printf_hex_aux(y);
+	return (j + 2);
+}
