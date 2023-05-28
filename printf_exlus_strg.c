8e8347a612ec97a2f323bd4d2ce3646c41f5a50f
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-28 11:37:07 -0700

    printf_exlus_strg.c

---
+++ printf_exlus_strg.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * printf_exclusive_string - print exclusiuve string.
+ * @val: argument.
+ * Return: the length of the string.
+ */
+
+int printf_exclusive_string(va_list val)
+{
+	char *s;
+	int w, len = 0;
+	int cast;
+
+	w = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (w = 0; s[w] != '\0'; w++)
+	{
+		if (s[w] < 32 || s[w] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[w];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + printf_HEX_aux(cast);
+		}
+		else
+		{
+			_putchar(s[w]);
+			len++;
+		}
+	}
+	return (len);
+}
