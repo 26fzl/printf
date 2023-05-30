0b83a8f15768969263def4b6bc4a6abae1bfe183
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-30 16:17:35 -0700

    print_ch.c

--- print_ch.c
+++ print_ch.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * printf_char - prints the char.
@@ -12,4 +13,24 @@ int printf_char(va_list val)
 	s = va_arg(val, int);
 	_putchar(s);
 	return (1);
+}
+
+/**
+ * print_s - Prints strings
+ * @s: Strings
+ *
+ * Return: The lenght of the string
+ */
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
